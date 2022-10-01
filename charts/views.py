079aad9f800fe179e312f101c2f8ec1c83457f6c
Edustartari <edustartari@gmail.com>
2022-10-01 11:09:15 -0300

    first commit

---
+++ charts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    print("")
+    print("index")
+    context = {}
+    return render(request, 'templates/front-end/index.html', context)
