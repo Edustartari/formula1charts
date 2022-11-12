3ff98ff9437a37b5542ba0ad5fe143bc97ac4530
Edustartari <edustartari@gmail.com>
2022-11-12 18:53:25 -0300

    plan how pilots page will look like

--- assets/src/js/containers/pilots.js
+++ assets/src/js/containers/pilots.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../css/season.css';
+import '../../css/pilots.css';
 import Image from '../components/image.js';
 import _ from "lodash";
 import {
@@ -17,7 +17,16 @@ class Pilots extends React.Component {
         const image = require(`../../img/f1_background_ferrari_2.jpg`);
         return(
             <div className='pilots-desktop-container'>
-                Testing pilots page
+                <div className='pilots-desktop-photos-list'>
+                    <div className='pilots-desktop-photo-card'>PILOT PHOTO</div>
+                    <div className='pilots-desktop-photo-card'>PILOT PHOTO</div>
+                    <div className='pilots-desktop-photo-card'>PILOT PHOTO</div>
+                    <div className='pilots-desktop-photo-card'>PILOT PHOTO</div>
+                    <div className='pilots-desktop-photo-card'>PILOT PHOTO</div>
+                </div>
+                <div className='pilots-desktop-chart'>CHART ONE</div>
+                <div className='pilots-desktop-chart'>CHART TWO</div>
+                <div className='pilots-desktop-menu'>SIDEBAR MENU</div>
             </div>
         )
     }
