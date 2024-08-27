19df80bec365bb15f4f26dfcac68f9a9881fd824
Eduardo <edustartari@gmail.com>
2024-08-27 16:20:28 -0300

    add config for vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Building project packages"
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Migrating Database"
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
-echo "COllecting static files"
-python manage.py collectstatic --noinput
+echo "Collecting static files"
+python3 manage.py collectstatic --noinput
