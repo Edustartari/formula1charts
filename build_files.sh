e336bfeb84fd67ef14ec82c2a375880f37fd2ff8
Eduardo <edustartari@gmail.com>
2024-08-27 16:57:39 -0300

    add config for vercel deployment 8

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Building project packages"
-python3 -m pip install pysqlite3==0.5.3
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Collecting static files"
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
