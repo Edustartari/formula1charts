2734994f52791fccd8f6c6c3acf02ff9efb56826
Eduardo <edustartari@gmail.com>
2024-08-27 16:40:39 -0300

    add config for vercel deployment 3

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Building project packages"
-sudo apt install libsqlite3-dev
+apt install libsqlite3-dev
 python3 -m pip install -r requirements.txt
 
 echo "Collecting static files"
