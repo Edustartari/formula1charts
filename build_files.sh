53b9dcd795becd41f30b7bbda8d065dd2765f057
Eduardo <edustartari@gmail.com>
2024-08-27 17:00:51 -0300

    add config for vercel deployment 10

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Building project packages"
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Collecting static files"
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
