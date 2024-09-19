b2f3d01c9dcd28e0dfd99e6dfd16d5b15a4b43cd
Eduardo <edustartari@gmail.com>
2024-09-18 23:54:28 -0300

    change wsgi config

--- formula1charts/wsgi.py
+++ formula1charts/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'formula1charts.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+app = DjangoWhiteNoise(application)
