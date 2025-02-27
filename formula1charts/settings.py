23cf13a949f67c58137dcd332c4adda65c4b1019
Eduardo <edustartari@gmail.com>
2025-02-26 23:54:46 -0300

    update allowed hosts array

--- formula1charts/settings.py
+++ formula1charts/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-^f!obxl*7lq^$*kcbjcpfw$3^ihck0h*h7m03a*quhn3lky)d*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'formula1charts.onrender.com']
 
 FILE_CHARSET = 'utf-16'
 
