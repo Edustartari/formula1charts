57e9acf0a09976a8e9ef6b54c351c5e79399f13c
Edustartari <edustartari@gmail.com>
2025-10-02 21:31:36 -0300

    update test file for test

--- cypress/e2e/test_pages.cy.js
+++ cypress/e2e/test_pages.cy.js
@@ -1,48 +1,50 @@
 /// <reference types="cypress" />
 
-describe('Season page', () => {
-  it('Enters Season page', function() {
-      cy.visit('/')
-      cy.get('[href="/all-seasons"]').click();
-      cy.get('#app-desktop-container .all-seasons-desktop-buttons div:nth-child(2)').click();
-      cy.get('#app-desktop-container .all-seasons-desktop-buttons div:nth-child(2)').click();
-      cy.get('#app-desktop-container [href="/season/2017"]').click();
-      cy.get('#home-desktop-menu-item-button button').click();
-      cy.get('[href="/"] [tabindex="0"] div span').click();    
-  });
-});
+// describe('Season page', () => {
+//   it('Enters Season page', function() {
+//       cy.visit('/')
+//       cy.get('[href="/all-seasons"]').click();
+//       cy.get('#app-desktop-container .all-seasons-desktop-buttons div:nth-child(2)').click();
+//       cy.get('#app-desktop-container .all-seasons-desktop-buttons div:nth-child(2)').click();
+//       cy.get('#app-desktop-container [href="/season/2017"]').click();
+//       cy.get('#home-desktop-menu-item-button button').click();
+//       cy.get('[href="/"] [tabindex="0"] div span').click();    
+//   });
+// });
 
-describe('Pilots Comparative page', () => {
-  it('Enters Pilots page and pick drivers', function() {
-      cy.visit('/')
-      cy.get('[href="/pilots"]', { timeout: 5000 }).click();
-      cy.get('#app-desktop-container .PrivateSwitchBase-input').uncheck();
-      cy.get('#app-desktop-container .pilots-desktop-menu-button').click();
-      cy.get('#app-desktop-container div:nth-child(2) > .pilots-desktop-photo-card-title > .material-icons').click();
-      cy.get('#standard-basic').click();
-      cy.get('#standard-basic').clear();
-      cy.get('#standard-basic').type('hamil');
-      cy.get('.pilots-overlay-input-filter div:nth-child(4)').click();
-      cy.get('.pilots-overlay-button').as('confirm_button').click();
-      cy.get('#app-desktop-container div:nth-child(3) .material-icons').click();
-      cy.get('#standard-basic').click();
-      cy.get('#standard-basic').clear();
-      cy.get('#standard-basic').type('prost');
-      cy.get('.pilots-overlay-input-filter div:nth-child(1)').click();
-      cy.get('@confirm_button').click();
-      cy.get('#home-desktop-menu-item-button button').click();
-      cy.get('[href="/"] [tabindex="0"] div span').click();    
-  });
-});
+// describe('Pilots Comparative page', () => {
+//   it('Enters Pilots page and pick drivers', function() {
+//       cy.visit('/')
+//       cy.get('[href="/pilots"]', { timeout: 5000 }).click();
+//       cy.get('#app-desktop-container .PrivateSwitchBase-input').uncheck();
+//       cy.get('#app-desktop-container .pilots-desktop-menu-button').click();
+//       cy.get('#app-desktop-container div:nth-child(2) > .pilots-desktop-photo-card-title > .material-icons').click();
+//       cy.get('#standard-basic').click();
+//       cy.get('#standard-basic').clear();
+//       cy.get('#standard-basic').type('hamil');
+//       cy.get('.pilots-overlay-input-filter div:nth-child(4)').click();
+//       cy.get('.pilots-overlay-button').as('confirm_button').click();
+//       cy.get('#app-desktop-container div:nth-child(3) .material-icons').click();
+//       cy.get('#standard-basic').click();
+//       cy.get('#standard-basic').clear();
+//       cy.get('#standard-basic').type('prost');
+//       cy.get('.pilots-overlay-input-filter div:nth-child(1)').click();
+//       cy.get('@confirm_button').click();
+//       cy.get('#home-desktop-menu-item-button button').click();
+//       cy.get('[href="/"] [tabindex="0"] div span').click();    
+//   });
+// });
 
 
 describe('Constructors page', () => {
     it('Enters Constructors page and interacts with filters', function() {
         cy.visit('/')
-        // cy.get('[href="/constructors"]', { timeout: 5000 }).click();
-        cy.get('#home-desktop-menu-item-button button').click();
-        cy.get('[href="/constructors"] [tabindex="0"] div span').click();
-        cy.get('#constructor-date-picker-to').click().type('1990');
+        cy.get('[href="/constructors"]', { timeout: 5000 }).click();
+        // Click on the element that has data-testid="CalendarIcon" inside #constructor-date-picker-to
+        cy.get('#constructor-date-picker-to [data-testid="CalendarIcon"]').click();
+        // Click on the button that has class class="MuiPickersYear-yearButton and the text 2006
+        cy.get('.MuiPickersYear-yearButton').contains('2006').click();
+        // cy.get('#constructor-date-picker-to').click({force: true}).type('1990');
         cy.get('body').click();
         cy.get('#constructors-accomplishments-select').click();
         cy.get('.MuiPaper-root').contains('Wins').click();
@@ -50,53 +52,53 @@ describe('Constructors page', () => {
     });
 });
 
-describe('Menu component', () => {
-    it('Try to enter to all options in menu', function() {
-        cy.visit('/')
-        cy.get('#home-desktop-menu-item-button button').click();
-        cy.get('[href="/all-seasons"] [tabindex="0"] div span').click();
-        cy.get('#home-desktop-menu-item-button button').click();
-        cy.get('[href="/pilots"] [tabindex="0"] div span').click();
-        cy.get('#home-desktop-menu-item-button button').click();
-        cy.get('[href="/all-time"] [tabindex="0"] div span').click();
-        cy.get('#home-desktop-menu-item-button button').click();
-        cy.get('[href="/constructors"] [tabindex="0"] div span').click();
-        cy.get('#home-desktop-menu-item-button button').click();
-        cy.get('[href="/others"] [tabindex="0"] div span').click();
-        cy.get('#home-desktop-menu-item-button button').click();
-        cy.get('[href="/"] [tabindex="0"] div span').click();
-    });
-});
+// describe('Menu component', () => {
+//     it('Try to enter to all options in menu', function() {
+//         cy.visit('/')
+//         cy.get('#home-desktop-menu-item-button button').click();
+//         cy.get('[href="/all-seasons"] [tabindex="0"] div span').click();
+//         cy.get('#home-desktop-menu-item-button button').click();
+//         cy.get('[href="/pilots"] [tabindex="0"] div span').click();
+//         cy.get('#home-desktop-menu-item-button button').click();
+//         cy.get('[href="/all-time"] [tabindex="0"] div span').click();
+//         cy.get('#home-desktop-menu-item-button button').click();
+//         cy.get('[href="/constructors"] [tabindex="0"] div span').click();
+//         cy.get('#home-desktop-menu-item-button button').click();
+//         cy.get('[href="/others"] [tabindex="0"] div span').click();
+//         cy.get('#home-desktop-menu-item-button button').click();
+//         cy.get('[href="/"] [tabindex="0"] div span').click();
+//     });
+// });
 
-describe('All Time page', () => {
-  it('Enters All Time page and interact with filters', function() {
-    cy.visit('/')
-    cy.get('[href="/all-time"]', { timeout: 5000 }).click();
-    cy.get('#app-desktop-container div:nth-child(2) > .all-time-chart-details > .barchart-background > .barchart-bars > div:nth-child(4)').click();
-    cy.get('body').click();
-    cy.get('.all-time-option-box-component-nationality-item').click();
-    cy.get('.MuiPaper-root').contains('Brazil').click();
-    cy.get('body').click();
-    cy.get('#number-of-drivers-select').click();
-    cy.get('.MuiPaper-root').contains('3').click();
-    cy.get('body').click();
-    cy.get('#accomplishments-select').click();
-    cy.get('.MuiPaper-root').contains('Wins').click();
-    cy.get('#app-desktop-container .all-time-search-button').click();
-    cy.get('body').click();
-    cy.get('#accomplishments-select').click();
-    cy.get('.MuiPaper-root').contains('Poles').click();
-    cy.get('#app-desktop-container .all-time-search-button').click();
-    cy.get('body').click();
-    cy.get('#accomplishments-select').click();
-    cy.get('.MuiPaper-root').contains('Podiums').click();
-    cy.get('#app-desktop-container .all-time-search-button').click();
-    cy.get('#date-picker-to').click().type('1990');
-    cy.get('#app-desktop-container .all-time-search-button').click();
-    cy.get('#home-desktop-menu-item-button button').click();
-    cy.get('[href="/"] [tabindex="0"] div span').click();
-  });
-});
+// describe('All Time page', () => {
+//   it('Enters All Time page and interact with filters', function() {
+//     cy.visit('/')
+//     cy.get('[href="/all-time"]', { timeout: 5000 }).click();
+//     cy.get('#app-desktop-container div:nth-child(2) > .all-time-chart-details > .barchart-background > .barchart-bars > div:nth-child(4)').click();
+//     cy.get('body').click();
+//     cy.get('.all-time-option-box-component-nationality-item').click();
+//     cy.get('.MuiPaper-root').contains('Brazil').click();
+//     cy.get('body').click();
+//     cy.get('#number-of-drivers-select').click();
+//     cy.get('.MuiPaper-root').contains('3').click();
+//     cy.get('body').click();
+//     cy.get('#accomplishments-select').click();
+//     cy.get('.MuiPaper-root').contains('Wins').click();
+//     cy.get('#app-desktop-container .all-time-search-button').click();
+//     cy.get('body').click();
+//     cy.get('#accomplishments-select').click();
+//     cy.get('.MuiPaper-root').contains('Poles').click();
+//     cy.get('#app-desktop-container .all-time-search-button').click();
+//     cy.get('body').click();
+//     cy.get('#accomplishments-select').click();
+//     cy.get('.MuiPaper-root').contains('Podiums').click();
+//     cy.get('#app-desktop-container .all-time-search-button').click();
+//     cy.get('#date-picker-to').click().type('1990');
+//     cy.get('#app-desktop-container .all-time-search-button').click();
+//     cy.get('#home-desktop-menu-item-button button').click();
+//     cy.get('[href="/"] [tabindex="0"] div span').click();
+//   });
+// });
 
 // describe('Others cool stats page', () => {
 //     it('Enter Others page and interact with filters', function() {
